ed8cecb031728e994461d8c2c562a7edb54ec1af
fzxlzy <810528574@qq.com>
2023-11-20 14:25:26 +0800

    补充license

--- harmony/netinfo/src/main/ets/NetInfoTurboModule.ts
+++ harmony/netinfo/src/main/ets/NetInfoTurboModule.ts
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANT KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { TurboModule } from 'rnoh/ts';
 import type { TurboModuleContext } from 'rnoh/ts';
 import wifiManager from '@ohos.wifiManager';
@@ -39,13 +63,17 @@ export class NetInfoTurboModule extends TurboModule {
     Logger.info('configure,' + JSON.stringify(config));
   }
 
-  getCurrentState(requestedInterface?: string): Promise<Object> {
+  getCurrentState(requestedInterface?: string): Promise<NetInfoState> {
     Logger.info('getCurrentState,' + requestedInterface);
     // this.ctx.rnInstance.emitDeviceEvent('netInfo.networkStatusDidChange', {})
     return new Promise(async (resolve, reject) => {
-      const events = await this.createConnectionEvent()
-      Logger.info('events,' + JSON.stringify(events));
-      resolve(events)
+      try {
+        const events = await this.createConnectionEvent()
+        Logger.info('events,' + JSON.stringify(events));
+        resolve(events)
+      } catch (e) {
+        reject(e)
+      }
     });
   }
 
--- src/internal/NativeRNCNetInfo.ts
+++ src/internal/NativeRNCNetInfo.ts
@@ -1,10 +1,37 @@
-/* eslint-disable @typescript-eslint/ban-types */
+/*
+ * MIT License
+ *
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANT KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
+import {NetInfoConfiguration} from './types';
+import {NetInfoNativeModuleState} from './privateTypes';
 
 export interface Spec extends TurboModule {
-  configure: (config: Object) => void;
-  getCurrentState(requestedInterface?: string): Promise<Object>;
+  //--no-ignore
+  configure: (config: Partial<NetInfoConfiguration>) => void;
+  //--no-ignore
+  getCurrentState(requestedInterface?: string): Promise<NetInfoNativeModuleState>;
   // Events
   addListener: (eventName: string) => void;
   removeListeners: (count: number) => void;
